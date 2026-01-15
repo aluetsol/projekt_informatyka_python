cd8dfd5db6d597b28404183deda6f8ccbf164821
aluetsol <anastasiiagerajmovich@gmail.com>
2026-01-15 01:35:51 +0100

    Dołączenie alarmu, zbiornika nr 4 oraz poprawiony wizual
    Updated the simulation to include a fourth tank and added alarm functionality for low and high liquid levels.

--- main.py
+++ main.py
@@ -62,6 +62,13 @@ class Zbiornik:
 
     def czy_pelny(self): return self.aktualna_ilosc >= self.pojemnosc - 0.1
 
+    def stan_alarmowy(self):
+        if self.poziom <= 0.05:
+            return "LOW"
+        if self.poziom >= 0.95:
+            return "HIGH"
+        return None
+    
     def punkt_gora_srodek(self): return (self.x + self.width / 2, self.y)
 
     def punkt_dol_srodek(self): return (self.x + self.width / 2, self.y + self.height)
@@ -74,20 +81,45 @@ class Zbiornik:
             painter.setBrush(QColor(0, 120, 255, 200))
             painter.drawRect(int(self.x + 3), int(y_start), int(self.width - 6), int(h_cieczy-2))
 
-        pen = QPen(Qt.white, 4)
+        alarm = self.stan_alarmowy()
+        if alarm == "HIGH":
+            pen = QPen(QColor(255, 140, 0), 4)
+        elif alarm == "LOW":
+            pen = QPen(QColor(255, 255, 0), 4)  
+        else:
+            pen = QPen(Qt.white, 4)
+
         pen.setJoinStyle(Qt.MiterJoin )
         painter.setPen(pen)
         painter.setBrush(Qt.NoBrush)
         painter.drawRect(self.x, self.y, self.width, self.height)
         painter.setPen(Qt.white)
+
+        font = painter.font()
+        font.setPointSize(9)
+        font.setBold(True)
+        painter.setFont(font)
         painter.drawText(self.x, self.y - 10, self.nazwa)
+    
+    def stan_alarmowy(self):
+        if self.poziom <= 0.05:
+            return "LOW"
+        if self.poziom >= 0.95:
+            return "HIGH"
+        return None
 
 class SymulacjaKaskady(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Kaskada : Dol -> Gora")
-        self.setFixedSize(900, 680)
-        self.setStyleSheet("background-color: #e5e5e5;")
+        self.setFixedSize(1200, 900)
+        self.setStyleSheet("""
+                           background-color: #F5DEB3;
+                           QPushButton {
+                                font-size: 14 px;
+                                font-weight: bold;
+                            }
+                           """)
 
         self.z1 = Zbiornik(50, 50, nazwa="Zbiornik 1")
         self.z1.aktualna_ilosc = 100
@@ -95,18 +127,26 @@ class SymulacjaKaskady(QWidget):
 
         self.z2 = Zbiornik(350, 200, nazwa="Zbiornik 2")
         self.z3 = Zbiornik(650, 350, nazwa="Zbiornik 3")
-        self.zbiorniki = [self.z1, self.z2, self.z3]
+        self.z4 = Zbiornik(350, 500, nazwa="Zbiornik 4")
+        self.zbiorniki = [self.z1, self.z2, self.z3, self.z4]
 
         self.rura1 = self.stworz_rure(self.z1, self.z2)
         self.rura2 = self.stworz_rure(self.z2, self.z3)
-        self.rury = [self.rura1, self.rura2]
+        self.rura3 = self.stworz_rure(self.z3, self.z4)
+        self.rury = [self.rura1, self.rura2, self.rura3]
         self.timer = QTimer()
         self.timer.timeout.connect(self.logika_przeplywu)
         self.running = False
         self.flow_speed = 1.0
+
         self.btn_start = QPushButton("Start / Stop", self)
-        self.btn_start.setGeometry(50, 540, 120, 30)
+        self.btn_start.setGeometry(50, 700, 120, 30)
         self.btn_start.clicked.connect(self.toggle)
+
+        self.btn_alarmy = QPushButton("Alarmy", self)
+        self.btn_alarmy.setGeometry(1000, 700, 120, 30)
+        self.btn_alarmy.clicked.connect(self.pokaz_alarmy)
+
         self.stworz_przyciski_reczne()
 
     def stworz_rure(self, z_gora, z_dol):
@@ -127,8 +167,8 @@ class SymulacjaKaskady(QWidget):
             btn_fill = QPushButton(f"+ {z.nazwa}", self)
             btn_empty = QPushButton(f"- {z.nazwa}", self)
 
-            btn_fill.setGeometry(x, 520, 130, 30) 
-            btn_empty.setGeometry(x , 570, 130, 30)
+            btn_fill.setGeometry(x, 680, 130, 30) 
+            btn_empty.setGeometry(x , 730, 130, 30)
             btn_fill.clicked.connect(lambda _, zb=z: self.napelnij(zb))
             btn_empty.clicked.connect(lambda _, zb=z: self.oproznij(zb))
 
@@ -166,7 +206,27 @@ class SymulacjaKaskady(QWidget):
             plynie2 = True
         self.rura2.ustaw_przeplyw(plynie2)
 
+        plynie3 = False
+        if self.z3.aktualna_ilosc > 5 and not self.z4.czy_pelny():
+            il = self.z3.usun_ciecz(self.flow_speed)
+            self.z4.dodaj_ciecz(il)
+            plynie3 = True
+        self.rura3.ustaw_przeplyw(plynie3)
+
         self.update()
+
+    def pokaz_alarmy(self):
+        msg = ""
+        for z in self.zbiorniki:
+            alarm = z.stan_alarmowy()
+            if alarm:
+                msg += f"{z.nazwa}: {alarm}\n"
+
+        if msg == "":
+            msg = "Brak aktywnych alarmów"
+
+        from PyQt5.QtWidgets import QMessageBox
+        QMessageBox.information(self, "Alarmy systemowe", msg)
 
     def paintEvent(self, event):
         painter = QPainter(self)
